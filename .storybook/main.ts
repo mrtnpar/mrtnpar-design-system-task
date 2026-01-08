cdecafd699f677ae6bcc72fdc0dcc3b60702857b
Martín Paredes <martin.paredes@gmail.com>
2026-01-08 16:39:55 +0900

    chore(storybook): upgrade to v10 and configure react-vite framework

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,17 +1,15 @@
 const config = {
-  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)', '../tests/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-    '@storybook/addon-themes',
+  stories: [
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
+    "../tests/**/*.stories.@(js|jsx|ts|tsx)",
   ],
-  framework: {
-    name: 'react',
-    options: {},
-  },
-  docs: {
-    autodocs: 'tag',
-  },
-}
-export default config
+
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-themes",
+    "@storybook/addon-docs",
+  ],
+
+  framework: "@storybook/react-vite",
+};
+export default config;
